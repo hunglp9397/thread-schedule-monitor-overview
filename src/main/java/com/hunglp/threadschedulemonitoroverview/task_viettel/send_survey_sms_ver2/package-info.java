1c88db65616b4e26678086d5f9d03acdca66839b
Le Phi Hung <101548961+hunglp9397@users.noreply.github.com>
2023-06-16 14:19:42 +0700

    Update package-info.java

--- src/main/java/com/hunglp/threadschedulemonitoroverview/task_viettel/send_survey_sms_ver2/package-info.java
+++ src/main/java/com/hunglp/threadschedulemonitoroverview/task_viettel/send_survey_sms_ver2/package-info.java
@@ -1,18 +1,18 @@
 package com.hunglp.threadschedulemonitoroverview.task_viettel.send_survey_sms_ver2;
 
 /*
-- Module "anti-business-call" cần gưi thông tin paydload (survey) sang module "sms-api"
+- Module "ABC" cần gưi thông tin paydload (khảo sát) sang module "sms"
 - Ta cần gửi 1 list các survey, Mỗi survey như sau : id, suspectPhone, surveyPhone, timeSpam
-- Luồng "sms-api"  cần phải thực hiện gửi bản tin và insert thông tin vô bảng survey
+- Luồng "sms"  cần phải thực hiện gửi bản tin và insert thông tin vô bảng khao sat
 
 
     ************************************************************************************
 - Trong ver2 nâng cấp này, về cơ bản cách làm như sau:
- + Tại Class MainApp giả sử gọi api /send Từ SurveyController để yêu cầu module sms-api thực hiện gửi survey
+ + MainAPP
  + Tại module sms api thực hiện:
     + Chia 3 việc: send, save, get thông tin
     + Tại mỗi việc, ghi thông tin ra file
-    + Sau khi làm xong 3 việc thì notify cho module "anti-business-call"
-    + Module "sms-api" sẽ trả 1 api về kết quả thoogn tin gửi thành công hay chưa để module "Anti-business-call" gọi
+    + Sau khi làm xong 3 việc thì notify cho module "ABC"
+    + Module "sms" sẽ trả 1 api về kết quả thoogn tin gửi thành công hay chưa để module "ABC" gọi
 
 */
